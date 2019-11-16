b6cf058e1a277121a3cfe6a68a6aff9775b5b17e
lixin <413318511@qq.com>
2019-11-16 19:10:34 +0800

    分页功能

--- public/assets/js/posts.js
+++ public/assets/js/posts.js
@@ -6,8 +6,10 @@ $.ajax({
 		// console.log(response);
 		// 根据模板引擎拼接html
 		let html = template("postsTpl", response);
+		let pageHtml = template("pageTpl", response);
 		// 渲染页面
 		$("#postsBox").html(html);
+		$("#pageBox").html(pageHtml);
 	}
 });
 
@@ -19,4 +21,23 @@ function dateFormate(date) {
 	return (
 		date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate()
 	);
+}
+
+// 分页功能
+function changePage(page) {
+	// 文章列表渲染
+	$.ajax({
+		type: "get",
+		url: "/posts",
+		data: { page: page },
+		success: function(response) {
+			// console.log(response);
+			// 根据模板引擎拼接html
+			let html = template("postsTpl", response);
+			let pageHtml = template("pageTpl", response);
+			// 渲染页面
+			$("#postsBox").html(html);
+			$("#pageBox").html(pageHtml);
+		}
+	});
 }
