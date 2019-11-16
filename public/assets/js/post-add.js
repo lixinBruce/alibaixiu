e698eec3b5f96d47d9daa892879b4e412f733294
lixin <413318511@qq.com>
2019-11-16 16:14:44 +0800

    文章封面上传功能

--- public/assets/js/post-add.js
+++ public/assets/js/post-add.js
@@ -9,3 +9,27 @@ $.ajax({
 		$("#category").html(html);
 	}
 });
+
+// 文章封面上传功能
+$("#feature").on("change", function() {
+	// 获取选择的文件
+	var file = this.files[0];
+	// 创建formdata对象
+	var formData = new FormData();
+	// 传入文件
+	formData.append("cover", file);
+	$.ajax({
+		type: "post",
+		url: "/upload",
+		data: formData,
+		// 不修改传入数据格式
+		processData: false,
+		// 不修改传入数据类型
+		contentType: false,
+		success: function(response) {
+			// console.log(response);
+			// 在隐藏域中保存图片路径
+			$("#thumbnail").val(response[0].cover);
+		}
+	});
+});
