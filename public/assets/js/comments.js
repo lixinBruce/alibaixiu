da7efac0cd171510a0ff56a0bca83c47e795cf21
lixin <413318511@qq.com>
2019-11-17 13:07:55 +0800

    评论分页功能

--- public/assets/js/comments.js
+++ public/assets/js/comments.js
@@ -6,6 +6,28 @@ $.ajax({
 		// console.log(response);
 		// 根据模板引擎拼接html
 		var html = template("commentsTpl", response);
+		var pageHtml = template("pageTpl", response);
+		// 渲染页面
 		$("#commentsBox").html(html);
+		$("#pageBox").html(pageHtml);
 	}
 });
+
+// 分页功能
+function changePage(page) {
+	// 评论列表渲染
+	$.ajax({
+		type: "get",
+		url: "/comments",
+		data: { page: page },
+		success: function(response) {
+			// console.log(response);
+			// 根据模板引擎拼接html
+			var html = template("commentsTpl", response);
+			var pageHtml = template("pageTpl", response);
+			// 渲染页面
+			$("#commentsBox").html(html);
+			$("#pageBox").html(pageHtml);
+		}
+	});
+}
